52ecd7d6ab39f78bcc1f71ee232adeccb7a96bb9
Manuel <hbiaser132@gmail.com>
2023-07-12 08:20:41 -0500

    fix: add suagger ui

--- src/utils/mainConfig.ts
+++ src/utils/mainConfig.ts
@@ -5,9 +5,11 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AllExceptionsHandler } from '#/src/AllExceptionsHandler/AllExceptionsHandler';
 
+import { Env } from './env';
 import { CustomLogger } from './logger';
 
 export function mainConfig(app: INestApplication) {
@@ -28,4 +30,22 @@ export function mainConfig(app: INestApplication) {
   app.useGlobalFilters(
     new AllExceptionsHandler(httpRef.httpAdapter.getHttpServer()),
   );
+
+  if (Env.nodeEnv === 'test') return;
+  const { SwaggerTheme } = require('swagger-themes');
+
+  const theme = new SwaggerTheme('v3');
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {});
+
+  SwaggerModule.setup('api', app, document, {
+    explorer: true,
+    customCss: theme.getBuffer('dark'),
+  });
 }
